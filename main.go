06161db3b3df440a8e6aaa3ec1a0735f14350d63
Reaper Gelera <sidgroks@gmail.com>
2021-03-28 05:30:10 +0530

    refactor(cleanup): cleanup and add comments
    adds comments to most needed handlers and also
    reduces the logic in each function

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ const postIndexPath = "./out/posts/index.html"
 const generateRSS = true
 const rssOutPath = "./out/blog.xml"
 
+// Metadata - meta data to be extracted from the frontmatter
 type Metadata struct {
 	Published bool   `yaml:"published"`
 	Title     string `yaml:"title"`
@@ -36,15 +37,22 @@ type Metadata struct {
 	Content   string
 }
 
+// Post - container for both metadata and the content of the post
+// used to differentiate between a post and just metadata as part of
+// other items that might need metadata
 type Post struct {
 	Meta    Metadata
 	Content string
 }
 
+// BlogIndex , used for storing metadata indexes for creatings a blog index
+// responsible for the /posts url to show a list of all available posts
 type BlogIndex struct {
 	Files []Metadata
 }
 
+// ATOMFeed - Information stuct containing all needed items for for creating a Atom RSS Feed
+// TODO: has duplicate structs and can reuse existing structs
 type ATOMFeed struct {
 	Site struct {
 		Name        string
@@ -71,11 +79,8 @@ func main() {
 	// Clean existing out directory
 	err := os.RemoveAll(outPath)
 
+	// Copy the public directory to the out folder for the needed public assets
 	err = CopyDir(publicFolder, outPath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -93,13 +98,18 @@ func main() {
 		),
 	)
 
+	// Parse all templates from templates directory to hold into
+	// the global variable to use later with other dynamic templates generated later
 	parsedTemplates, err = template.ParseGlob(templatesPath + "/*")
 
+	// Convert the given content source from config into markdown/compiled HTML files
 	err = convertDirectoryToMarkdown(contentPath)
 	if err != nil {
 		log.Fatal("Failed to convert directory, Error: ", err)
 	}
 
+	// Create a blog index file and write contents to it
+	// using the existing blog index template
 	blogIndexFile, err := os.Create(postIndexPath)
 	defer blogIndexFile.Close()
 
@@ -110,6 +120,7 @@ func main() {
 	err = parsedTemplates.ExecuteTemplate(blogIndexFile, "blogIndexHTML", BlogIndex{Files: filesForIndex})
 	blogIndexFile.Sync()
 
+	// Generate the rss feed if the config variable is set to true
 	if generateRSS {
 		feed := ATOMFeed{}
 
@@ -158,10 +169,13 @@ func main() {
 
 }
 
+// changeFileExtension - simple replace statement wrapper to change the extension of files
 func changeFileExtension(fileName string, checkFor string, replaceWith string) string {
 	return strings.Replace(fileName, checkFor, replaceWith, 1)
 }
 
+// writeToBlog - Write the given html to the blog output
+// and compile the html using the given the metadata
 func writeToBlog(fileNameHTML string, metadata Metadata) {
 	fileToWrite, err := os.Create(outPath + "/" + fileNameHTML)
 	if err != nil {
@@ -182,6 +196,8 @@ func writeToBlog(fileNameHTML string, metadata Metadata) {
 	fileToWrite.Sync()
 }
 
+// writeToPage - write to a simple page instead of blog and use the simple
+// page template to create the output file, again uses metadata as needed
 func writeToPage(fileNameHTML string, content []byte, metadata Metadata) {
 	fileToWrite, err := os.Create(outPath + "/" + fileNameHTML)
 	if err != nil {
@@ -208,11 +224,15 @@ func writeToPage(fileNameHTML string, content []byte, metadata Metadata) {
 	fileToWrite.Sync()
 }
 
+// Convert the given directory into markdown and write the processed data to
+// the out folder
 func convertDirectoryToMarkdown(srcFolder string) error {
-
+	// define prefixes for reading and writing
 	pathPrefix := srcFolder
 	outPathPrefix := "/"
 
+	// if the source folder and the content folder don't match,
+	// aka, you are not on the root directory for processing so add the nested values to the path
 	if srcFolder != contentPath {
 		pathPrefix = contentPath + "/" + srcFolder
 		outPathPrefix = strings.Replace(srcFolder, "./", "", 1)
@@ -238,79 +258,134 @@ func convertDirectoryToMarkdown(srcFolder string) error {
 			continue
 		}
 
-		extension := strings.SplitN(file.Name(), ".", -1)
-		isHTMLFile := extension[len(extension)-1] == "html"
-
-		os.MkdirAll(outPath+"/"+outPathPrefix, os.ModePerm)
-
-		fileData, err := ioutil.ReadFile(pathPrefix + "/" + file.Name())
-
-		if extension[len(extension)-1] != "md" && !isHTMLFile {
-			log.Println("Skipping file, not a markdown file", file.Name())
-			continue
-		}
-
-		var parsedHtmlFile *template.Template
-		var dynTemplateName string
-		if isHTMLFile {
-			var err error
-			dynTemplateName = file.Name() + "HTML"
-			newTemplate := parsedTemplates.New(file.Name() + "HTML")
-			if err != nil {
-				return err
-			}
-			parsedHtmlFile, err = newTemplate.Parse(string(fileData))
-			if err != nil {
-				return err
-			}
-		}
-
+		err := handleUnprocessedTemplate(pathPrefix, outPathPrefix, file)
 		if err != nil {
-			log.Println(err)
-		}
-
-		metadata := Metadata{}
-
-		parts := bytes.SplitN(fileData, []byte("---"), 3)
-
-		fileNameHTML := changeFileExtension(file.Name(), ".md", ".html")
-
-		metadata.Slug = outPathPrefix + "/" + fileNameHTML
-
-		if len(parts) == 3 {
-			err = yaml.Unmarshal(parts[1], &metadata)
-
-			if err != nil {
-				log.Println(err)
-			}
-
-			if metadata.Published {
-				var toHTML bytes.Buffer
-
-				err = markdownProcessor.Convert(parts[2], &toHTML)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				metadata.Content = toHTML.String()
-				filesForIndex = append(filesForIndex, metadata)
-				writeToBlog(outPathPrefix+"/"+fileNameHTML, metadata)
-			}
-		} else {
-			if isHTMLFile {
-				writeParsedHTML(outPath+"/"+outPathPrefix+"/"+fileNameHTML, parsedHtmlFile, dynTemplateName)
-			} else {
-				simpleFileName := changeFileExtension(file.Name(), ".md", "")
-				simpleFileName = strings.ReplaceAll(simpleFileName, "-", "")
-				metadata.Title = toTitleCase(simpleFileName)
-				writeToPage(outPathPrefix+"/"+fileNameHTML, fileData, metadata)
-			}
+			return err
 		}
 	}
 
 	return nil
 }
 
+// Check if the extension matches that of a markdown file
+func isMarkdownFile(file os.FileInfo) bool {
+	extension := strings.SplitN(file.Name(), ".", -1)
+	return extension[len(extension)-1] == "md"
+}
+
+// Check if the extension matches that of a html file
+func isHTMLFile(file os.FileInfo) bool {
+	extension := strings.SplitN(file.Name(), ".", -1)
+	return extension[len(extension)-1] == "html"
+}
+
+// normalizeOtherFileTypeName - as the name specifies, make sure the name doesn't have any spaces when
+// used to create a html file
+func normalizeOtherFileTypeName(file os.FileInfo) string {
+	fileName := changeFileExtension(file.Name(), ".md", "")
+	fileName = strings.ReplaceAll(fileName, "-", "")
+	return fileName
+}
+
+//  handleUnprocessedTemplate - handle the given path, output , file to process
+//  by converting an Markdown file with Frontmatter into a blog file
+//  a normal markdown file into a simple html file and
+//  a html file into a dynamically compiled go html template
+func handleUnprocessedTemplate(pathPrefix string, outPathPrefix string, file os.FileInfo) error {
+	var err error
+	var isHTMLFileBool = isHTMLFile(file)
+
+	os.MkdirAll(outPath+"/"+outPathPrefix, os.ModePerm)
+
+	fileData, err := ioutil.ReadFile(pathPrefix + "/" + file.Name())
+
+	if !isMarkdownFile(file) && !isHTMLFileBool {
+		log.Println("Skipping file, not a markdown file", file.Name())
+		return nil
+	}
+
+	if isHTMLFileBool {
+		err = handleHTMLFile(file, fileData, outPathPrefix)
+		if err != nil {
+			return err
+		}
+	} else if isMarkdownWithFrontMatter(fileData) {
+		return handleMarkdownFile(file, fileData, outPathPrefix)
+	} else {
+		return handleOtherFile(file, fileData, outPathPrefix)
+	}
+	return nil
+}
+
+// isMarkdownWithFrontMatter - check if the markdown file has any frontmatter
+func isMarkdownWithFrontMatter(fileData []byte) bool {
+	parts := bytes.SplitN(fileData, []byte("---"), 3)
+	if len(parts) != 3 {
+		return false
+	}
+	return true
+}
+
+//  handleOtherFile - handle files that are not html templates or blog (with frontmatter)
+//  based markdown and just a simple markdown file
+func handleOtherFile(file os.FileInfo, fileData []byte, outPathPrefix string) error {
+	metadata := Metadata{}
+	fileNameHTML := changeFileExtension(file.Name(), ".md", ".html")
+	metadata.Slug = outPathPrefix + "/" + fileNameHTML
+	name := normalizeOtherFileTypeName(file)
+	metadata.Title = toTitleCase(name)
+	writeToPage(outPathPrefix+"/"+fileNameHTML, fileData, metadata)
+	return nil
+}
+
+// handleHTMLFile - handle creation and writing of a html file by dynamically creating
+// a template , compiling it and then writing it with the same name as the source
+// used to create the compiled index.html page of this blog
+func handleHTMLFile(file os.FileInfo, fileData []byte, outPathPrefix string) error {
+	fileNameHTML := changeFileExtension(file.Name(), ".md", ".html")
+	dynTemplateName := file.Name() + "HTML"
+	newTemplate := parsedTemplates.New(file.Name() + "HTML")
+	parsedHtmlFile, err := newTemplate.Parse(string(fileData))
+	if err != nil {
+		return err
+	}
+	writeParsedHTML(outPath+"/"+outPathPrefix+"/"+fileNameHTML, parsedHtmlFile, dynTemplateName)
+	return nil
+}
+
+// handleMarkdownFile - specifically handle markdown files with frontmatter available
+// and pass them to compile with the blog-post styled template
+func handleMarkdownFile(file os.FileInfo, fileData []byte, outPathPrefix string) error {
+	metadata := Metadata{}
+	fileNameHTML := changeFileExtension(file.Name(), ".md", ".html")
+	metadata.Slug = outPathPrefix + "/" + fileNameHTML
+	parts := bytes.SplitN(fileData, []byte("---"), 3)
+	if len(parts) != 3 {
+		return nil
+	}
+
+	err := yaml.Unmarshal(parts[1], &metadata)
+
+	if err != nil {
+		return err
+	}
+
+	if metadata.Published {
+		var toHTML bytes.Buffer
+
+		err = markdownProcessor.Convert(parts[2], &toHTML)
+		if err != nil {
+			return err
+		}
+
+		metadata.Content = toHTML.String()
+		filesForIndex = append(filesForIndex, metadata)
+		writeToBlog(outPathPrefix+"/"+fileNameHTML, metadata)
+	}
+	return nil
+}
+
+// writeParsedHTML - write the parsed html file to the needed out file
 func writeParsedHTML(filePath string, templates *template.Template, templateName string) {
 	fileToWrite, err := os.Create(filePath)
 	if err != nil {
@@ -321,6 +396,8 @@ func writeParsedHTML(filePath string, templates *template.Template, templateName
 	fileToWrite.Sync()
 }
 
+// toTitleCase - simple string convertion for strings to title case style
+// eg: a fox => A Fox
 func toTitleCase(toConv string) string {
 	parts := strings.SplitN(toConv, " ", -1)
 	result := []string{}
