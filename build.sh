2d2f0b61260868887401d7d51448aab4c426f8c2
Reaper <ahoy@barelyhuman.dev>
2022-01-21 13:38:58 +0530

    landing changed to redirect

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -sSL "https://goblin.reaper.im/github.com/barelyhuman/statico" | sh; statico;
 
 # Make sure you have node and npx installed
 npx purgecss --config purgecss.config.js
