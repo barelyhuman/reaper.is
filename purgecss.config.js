39d490247e271a0645b052a2b8a55cd801623970
Reaper <ahoy@barelyhuman.dev>
2022-01-30 15:13:17 +0530

    add custom extractor

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,15 @@
+const extractor = (content) => {
+  return content.match(/[A-z0-9-:.\/]+/g);
+};
+
 module.exports = {
   content: ["out/**/*.html"],
   css: ["out/styles/*.css"],
   output: "./out/styles",
+  extractors: [
+    {
+      extractor: extractor,
+      extensions: ["html"],
+    },
+  ],
 };
