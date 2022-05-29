4e117311d474c100a643a18b62f6bb7d5ef5e14a
Reaper <ahoy@barelyhuman.dev>
2022-05-29 16:55:25 +0530

    add description handling for rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,18 +3,22 @@ import dateParser from "../api/posts";
 
 const postImportResult = import.meta.globEager("./writing/**/*.md");
 const posts = Object.values(postImportResult);
+const site = import.meta.env.SITE || "https://reaper.im";
 
 export const get = () =>
   rss({
     title: "Reaper's Rants,Notes and Stuff",
     description: "Just another developer's public space",
-    site: import.meta.env.SITE || "https://reaper.im",
+    site,
     items: posts.map((postItem) => {
-        console.log({postItem})
-        return {
-            link:postItem.url,
-            title: postItem.frontmatter.title,
-            pubDate: dateParser(postItem.frontmatter.date),
-          }
+      const description =
+        postItem.rawContent && postItem.rawContent().split(" ").slice(0, 100).join(" ") + `...`;
+        
+      return {
+        link: postItem.url,
+        description,
+        title: postItem.frontmatter.title,
+        pubDate: dateParser(postItem.frontmatter.date),
+      };
     }),
   });
