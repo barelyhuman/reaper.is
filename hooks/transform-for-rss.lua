9bc49b263e9e50f9ae1a2314de8d3719332873f5
Reaper <ahoy@barelyhuman.dev>
2022-08-04 22:03:19 +0530

    base template and post processing base script

--- hooks/transform-for-rss.lua
+++ hooks/transform-for-rss.lua
@@ -3,12 +3,17 @@ package.path = package.path .. ";../lib/?.lua"
 local json = require("json")
 
 
-function Writer(filedata)
+-- will run but after the initial processing is done with
+-- still not implemented in alvu, so this still needs work
+function PostWriter(filedata)
     local source_data = json.decode(filedata);
-
     if source_data.name == "rss.xml"
     then 
-        print("yo, rss here");
+        source_data.data = {
+            name="reaper's notes,rants, and other stuff",
+            description ="just another developer crying about stuff he doesn't wanna change",
+            link="reaper.is",
+        }
     end
 
     return json.encode(source_data)
