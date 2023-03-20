a2203e32a749c60359f1332fb64fe38758b55de8
reaper <ahoy@barelyhuman.dev>
2023-03-20 12:08:04 +0530

    add homepage post

---
+++ hooks/add-home-post.lua
@@ -0,0 +1,63 @@
+package.path = package.path .. ";../lib/?.lua"
+
+ForFile = "index.md"
+
+local lib = require("lib.utils")
+local yaml = require("yaml")
+local strings = require("strings")
+local json = require("json")
+local alvu = require("alvu")
+
+local function sortbydate(postone, posttwo)
+	return postone.date > posttwo.date
+end
+
+function Writer(filedata)
+	local basePath = "pages/writing"
+	local files = alvu.files(basePath)
+	local meta = {}
+
+	for file = 1, #files do
+		if not string.find(files[file], "index.md") then
+			local name = string.gsub(files[file], ".md", "")
+			name = string.gsub(name, ".html", "")
+
+			local filecontent = lib.getfiledata(basePath .. "/" .. files[file])
+
+			if filecontent then
+				local match = strings.split(filecontent, "---")
+
+				if match[2] then
+					local frontmatterParsed = yaml.decode(match[2])
+					local date = lib.parse_dates(frontmatterParsed.date)
+					if not frontmatterParsed.rss_only then
+						table.insert(meta, {
+							content = match[3],
+							slug = name,
+							title = frontmatterParsed.title,
+							date = date,
+							formatteddate = os.date("%d-%m-%Y", date),
+						})
+					end
+				end
+			end
+		end
+	end
+
+	table.sort(meta, sortbydate)
+
+	local source_data = json.decode(filedata)
+	local lastPost = meta[1]
+
+  
+
+	source_data = {
+		content = "# " .. lastPost.title .. "\n" .. lastPost.content,
+		meta = {
+			title = lastPost.title,
+			date = lastPost.date,
+		},
+	}
+
+	return json.encode(source_data)
+end
---
+++ pages/about.md
@@ -0,0 +1,33 @@
+Hey, I'm <h1>Reaper</h1>
+
+**Principal Developer** at [Fountane](https://fountane.com)
+
+Mostly seen building tools and libraries to complicate my own life while trying
+to ease it for my future self.
+
+Dealing with chaos and tinkering with different programming languages and
+standards are my favorite hobbies. I don't have a preferred programming language
+and most of my work revolves around web and cli tools.
+
+**My work could speak for itself** (though it doesn't speak much...)
+
+&rarr; [Github](https://github.com/barelyhuman)
+
+Here's a few ways you to get in touch, feel free to reach out for help.
+
+- **Twitter**: [barelyreaper](https://twitter.com/barelyreaper)
+- **Mail**: [ahoy@barelyhuman.dev](mailto:ahoy@barelyhuman.dev)
+
+What am I doing [now](/now)?
+
+### Newsletter
+
+<section>
+<p>
+If you like the posts on here and wish to get notified via email for every new post, please
+subscribe to
+the newsletter. Though, I'd recommend getting an <strong>RSS reader</strong> since it's a lot less
+distractive.
+</p>
+<a href="https://buttondown.email/barelyhuman/"> Subscribe </a>
+</section>
